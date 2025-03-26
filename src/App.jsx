d3a37c516e56041949c1333f0402081093dad8ac
ssatpanha <satpanha618@gmail.com>
2025-03-26 13:33:35 +0700

    update about us

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,11 @@ import userProfile from './assets/profile.avif';
 import VideoPlayer from './components/VideoPlayer'
 import CourseView from './components/CourseView';
 import { PrimeReactProvider } from 'primereact/api';
-import "primereact/resources/themes/lara-light-blue/theme.css"; // Theme
-import "primereact/resources/primereact.min.css"; // Core styles
-import "primeicons/primeicons.css"; // Icons
-import "primeflex/primeflex.css"; // Utility classes
-import CourseBoxLearn from './components/CourseBoxLearn';
-import { ProgressBar } from 'primereact/progressbar'; 
-import DropDown from './components/DropDown';
-import './index.css';
 import "primereact/resources/themes/lara-light-blue/theme.css"; 
 import "primereact/resources/primereact.min.css"; 
 import "primeicons/primeicons.css"; 
-import "primeflex/primeflex.css"; 
-import {Route,Routes} from 'react-router-dom'
+import Quizz from './components/Quizz'
+import { Routes,Route } from 'react-router-dom';
 
 export default function App() {
   let userName = "Cheng Nhajor"
